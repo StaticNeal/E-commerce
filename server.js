562340b042f11df1ff8c2067bc76206721c04589
Idion-san <dineshramm85@gmail.com>
2026-04-19 09:02:41 +0530

    2nd commit

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('hello');
+//module imports
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express()
+
+// Set up views engine and path
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('pages/index', { title: 'Home' });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`running: http://localhost:${process.env.PORT || 5000}`);
+});             
