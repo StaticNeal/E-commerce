6c4079aff516f07fb58bfd6b48814fa2b0b2b3e0
Idion-san <dineshramm85@gmail.com>
2026-05-02 09:28:53 +0530

    v14

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -4,6 +4,15 @@ const settingsBtn = document.querySelector('.menu-btn');
 const cart = document.querySelector('.cart');
 const settings = document.querySelector('.settings');
 
+checkSidebar();
+
+function checkSidebar() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+        sidebar.classList.add('hidden');
+    }
+}
+
 function togglePanel(panelToOpen, panelToHide) {
     const isHidden = panelToOpen.classList.contains('hidden');
 
@@ -11,9 +20,36 @@ function togglePanel(panelToOpen, panelToHide) {
         sidebar.classList.remove('hidden');
         panelToOpen.classList.remove('hidden');
         panelToHide.classList.add('hidden');
+        try{
+            const overlay = document.querySelector('.overlay');
+            if(overlay){
+                overlay.classList.remove('hidden');
+            }        }catch(error){
+            console.error('Error toggling overlay:', error);
+        }
     } else {
         sidebar.classList.add('hidden');
         panelToOpen.classList.add('hidden');
+        try{
+            const overlay = document.querySelector('.overlay');
+            if(overlay){
+                overlay.classList.add('hidden');
+            }
+        }catch(error){
+            console.error('Error toggling overlay:', error);
+        }
+    }
+}
+
+function toggleSidebar() {
+    sidebar.classList.toggle('hidden');
+    try{
+        const overlay = document.querySelector('.overlay');
+        if(overlay){
+            overlay.classList.toggle('hidden');
+        }
+    }catch(error){
+        console.error('Error toggling overlay:', error);
     }
 }
 
